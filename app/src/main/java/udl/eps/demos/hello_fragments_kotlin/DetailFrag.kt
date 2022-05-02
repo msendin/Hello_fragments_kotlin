b3c398941cb5e64f4b0cafb391269895c87fde98
Montse <msendinv@gmail.com>
2022-05-02 18:48:17 +0200

    Comments removed

--- app/src/main/java/udl/eps/demos/hello_fragments_kotlin/DetailFrag.kt
+++ app/src/main/java/udl/eps/demos/hello_fragments_kotlin/DetailFrag.kt
@@ -4,42 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-//import androidx.databinding.DataBindingUtil
+
 import androidx.fragment.app.Fragment
-//import udl.eps.demos.hello_fragments_kotlin.databinding.FragmentDetailBinding
+import udl.eps.demos.hello_fragments_kotlin.databinding.DetailFragmentBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFrag.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var binding: DetailFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.detail_fragment, container, false)
-
-        //val binding = DataBindingUtil.inflate<DetailFragmentBinding>(inflater,
-           // R.layout.detail_fragment,container,false)
-        //return binding.root
+        binding = DetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     fun showText(item: String?) {
-        val view = requireView().findViewById<TextView>(R.id.captain)
-        view.text = item
+        binding.captain.text = item
     }
-
 }
--- app/src/main/java/udl/eps/demos/hello_fragments_kotlin/MainActivity.kt
+++ app/src/main/java/udl/eps/demos/hello_fragments_kotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package udl.eps.demos.hello_fragments_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import udl.eps.demos.hello_fragments_kotlin.databinding.ActivityMainBinding
+import udl.eps.demos.hello_fragments_kotlin.databinding.DetailFragmentBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val frag = childFragmentManager.findFragmentById(binding.frag_capt.id) as Fragment
+
+        //setContentView(R.layout.activity_main)
     }
 }
